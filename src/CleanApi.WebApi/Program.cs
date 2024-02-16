5103587a8db48caaa0acdff3d10ace55cf2e735e
EduardoPPCaldas <eduardocaldas.dev@gmail.com>
2024-02-16 01:11:30 -0300

    Feat: Separate weather forecast api in clean arch

--- src/CleanApi.WebApi/Program.cs
+++ src/CleanApi.WebApi/Program.cs
@@ -2,6 +2,7 @@ using CleanApi.Application;
 using CleanApi.Domain;
 using CleanApi.Infrastructure;
 using CleanApi.WebApi;
+using CleanApi.WebApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,31 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.AddWeatherForecastEndpoints();
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
